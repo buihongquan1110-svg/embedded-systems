86989d715b76432d0a2fd0c32cd1f4d3761fe933
buihongquan1110-svg <buihongquan1110@gmail.com>
2025-10-19 17:39:30 +0700

    Add files via upload

---
+++ FreeRTOS_Main-main/User/freertos.h
@@ -0,0 +1,8 @@
+#ifndef FREERTOS_APP_H
+#define FREERTOS_APP_H
+
+#include "cmsis_os.h"
+
+void MX_FREERTOS_Init(void);
+
+#endif
---
+++ FreeRTOS_Main-main/User/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+#include "cmsis_os.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
