46eee79d5be0a8bf93ba5a812a43a1b6ba6f8373
buihongquan1110-svg <buihongquan1110@gmail.com>
2025-10-26 00:03:29 +0700

    Delete User directory

--- User/freertos.c
+++
@@ -1,7 +0,0 @@
-#include "freertos.h"
-#include "cmsis_os.h"
-
-void MX_FREERTOS_Init(void)
-{
-  /* This file is a stub because tasks are created directly in main.c for this demo. */
-}
--- User/freertos.h
+++
@@ -1,8 +0,0 @@
-#ifndef FREERTOS_APP_H
-#define FREERTOS_APP_H
-
-#include "cmsis_os.h"
-
-void MX_FREERTOS_Init(void);
-
-#endif
